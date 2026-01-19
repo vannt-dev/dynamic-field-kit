c0ca5e091f8bbdcd5352ed1cb52f36abe8f63d81
Van Nguyen <nguyenthanhvan1902@gmail.com>
2026-01-19 15:37:02 +0700

    add layout

--- packages/react/src/MultiFieldInput.tsx
+++ packages/react/src/MultiFieldInput.tsx
@@ -2,16 +2,20 @@ import { FieldDescription, Properties } from "@dynamic-field-kit/core"
 import { Fragment, useEffect, useMemo, useState } from "react"
 import FieldInput from "./FieldInput"
 
+type Layout = "row" | "column" | "grid"
+
 interface Props {
   fieldDescriptions: FieldDescription[]
   properties?: Properties
   onChange?: (data: Properties) => void
+  layout?: Layout
 }
 
 const MultiFieldInput = ({
   fieldDescriptions,
   properties,
   onChange,
+  layout = "column",
 }: Props) => {
   const [data, setData] = useState<Properties>({})
 
@@ -27,22 +31,30 @@ const MultiFieldInput = ({
     [fieldDescriptions, data]
   )
 
+  const containerStyle: React.CSSProperties = {
+    display: layout === "grid" ? "grid" : "flex",
+    flexDirection: layout === "row" ? "row" : "column",
+    gap: 12,
+    ...(layout === "grid" && {
+      gridTemplateColumns: "repeat(2, 1fr)",
+    }),
+  }
+
   return (
-    <>
+    <div style={containerStyle}>
       {visibleFields.map((f) => (
-        <Fragment key={f.name}>
-          <FieldInput
-            fieldDescription={f}
-            renderInfos={data}
-            onValueChangeField={(value, key) => {
-              const next = { ...data, [key]: value }
-              setData(next)
-              onChange?.(next)
-            }}
-          />
-        </Fragment>
+        <FieldInput
+          key={f.name}
+          fieldDescription={f}
+          renderInfos={data}
+          onValueChangeField={(value, key) => {
+            const next = { ...data, [key]: value }
+            setData(next)
+            onChange?.(next)
+          }}
+        />
       ))}
-    </>
+    </div>
   )
 }
 
