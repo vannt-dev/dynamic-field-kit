3ad78a137aa0b6e3a80e77b9e7aa5a238159aa42
Van Nguyen <nguyenthanhvan1902@gmail.com>
2026-01-19 14:31:59 +0700

    update export type in react

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,11 @@
-export * from "./types"
+// 🔁 Re-export từ core (CÙNG INSTANCE ĐÃ BUNDLE)
+export {
+  fieldRegistry,
+  type FieldTypeKey,
+  type FieldDescription,
+  type FieldRendererProps,
+} from "@dynamic-field-kit/core"
+
 export { default as DynamicInput } from "./DynamicInput"
 export { default as FieldInput } from "./FieldInput"
 export { default as MultiFieldInput } from "./MultiFieldInput"
