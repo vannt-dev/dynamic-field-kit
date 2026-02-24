228a2c12dea2dd99aed7f30e90315543baaf0966
Van Nguyen <nguyenthanhvan1902@gmail.com>
2026-02-24 16:50:39 +0700

    update build version public

--- packages/core/src/FieldRegistry.ts
+++ packages/core/src/FieldRegistry.ts
@@ -4,10 +4,10 @@ import type { FieldRendererProps, FieldTypeMap } from "./types"
 export type FieldRenderer<T = any> = (props: FieldRendererProps<T>) => JSX.Element
 
 export class FieldRegistry {
-    // ❗ INTERNAL = string, KHÔNG constrain
+    // ❗ INTERNAL = string, not constrain
     private registry: Record<string, FieldRenderer<any>> = {}
 
-    // ✅ Type safety ở API
+    // ✅ Type safety API
     register<K extends keyof FieldTypeMap>(
         type: K,
         renderer: FieldRenderer<FieldTypeMap[K]>
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,2 @@
 export * from "./types"
-export * from "./fieldRegistry"
+export * from "./FieldRegistry"
